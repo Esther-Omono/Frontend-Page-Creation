53baaebdd86e23e31d57201f68366c03f610055c
Esther-Omono <enitanatakere@gmail.com>
2023-09-07 23:29:46 +0100

    Added element to display current UTC time in milliseconds

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+const currentDayAndTime = new Date();
+
 // To get the Current Day Of The Week
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const currentDay = new Date();
-let day = weekday[currentDay.getDay()];
+let day = weekday[currentDayAndTime.getDay()];
 document.getElementById("current-day").innerHTML = day;
 
-// 
+// To get the UTC Time in milliseconds
+let time = currentDayAndTime.getUTCMilliseconds();
+document.getElementById("current-time").innerHTML = time;
